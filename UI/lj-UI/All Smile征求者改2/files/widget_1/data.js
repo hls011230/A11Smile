c0f44e6af196eb2dcc2bc87e9cb94be90dbb89c7
unknown <1621967005@qq.com>
2022-03-25 23:55:51 +0800

    UI

---
+++ UI/lj-UI/All Smile征求者改2/files/widget_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(p,q),r,[s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,N,N,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,ba,_(H,I,J,bb),bc,S,bd,Z,be,_(bf,bg,bh,bi,bj,bi,bk,bi,bl,bm,J,_(bn,bo,bp,bo,bq,bo,br,bs)),i,_(j,bm,l,bm)),bt,_(),bu,_(),bv,_(bw,[])),bx,_(),by,_());}; 
+var b="url",c="widget_1.html",d="generationDate",e=new Date(1648209986817.6665),f="defaultAdaptiveView",g="name",h="iPhone 13 / 13 Pro / 12 / 12 Pro",i="size",j="width",k=390,l="height",m=844,n="adaptiveViews",o="sketchKeys",p="",q="s0",r="variables",s="OnLoadVariable",t="page",u="packageId",v="76bfc06ad1f149f8a661807fbd5a604a",w="type",x="Axure:Page",y="Widget 1",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="center",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="near",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="all",ba="borderFill",bb=0xFF797979,bc="cornerRadius",bd="cornerVisibility",be="outerShadow",bf="on",bg=false,bh="offsetX",bi=5,bj="offsetY",bk="blurRadius",bl="spread",bm=0,bn="r",bo=0,bp="g",bq="b",br="a",bs=0.34901960784313724,bt="adaptiveStyles",bu="interactionMap",bv="diagram",bw="objects",bx="masters",by="objectPaths";
+return _creator();
+})());
