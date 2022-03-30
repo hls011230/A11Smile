0ef77d0a486eb28cc8d1381a2184c6e5a63c0b15
xyj <1942049754@qq.com>
2022-03-30 14:11:50 +0800

    xyj 图片上传云端

--- mini_program/weapp/pages/user_uploading/user_uploading.js
+++ mini_program/weapp/pages/user_uploading/user_uploading.js
@@ -1,4 +1,10 @@
 // pages/user_uploading/user_uploading.js
+
+wx.cloud.init({
+    env: 'prod-9gy59jvo10e0946b', //填上你的云开发环境id
+    traceUser: true,
+  })
+  const db = wx.cloud.database()
 Page({
 
     /**
@@ -7,6 +13,51 @@ Page({
     data: {
 
     },
+
+        //JS文件   上传图片函数
+upload_picture: function(name) {
+    var that = this
+    //让用户选择或拍摄一张照片
+    wx.chooseImage({
+      count: 1,	
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+      //选择完成会先返回一个临时地址保存备用
+        const tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        //将照片上传至云端需要刚才存储的临时地址
+        wx.cloud.uploadFile({
+          cloudPath: 'images/test/'+new Date().getTime() + "_" +  Math.floor(Math.random()*1000)+'.jpg',
+          filePath: tempFilePaths[0],
+          success(res) {
+              console.log(res.fileID)
+              console.log('上传图片成功')
+              wx.showToast({
+                title: '上传成功',
+                icon: 'success',
+                duration: 1000,
+                success: function(){
+                  setTimeout(
+                    function () {
+                      wx.switchTab({
+                          url: '/pages/user_data/user_data',
+                      })
+                    },1000)
+                }
+              })
+          },
+          fail(res) {
+              wx.showToast({
+                title: '上传失败',
+                icon: 'fail',
+                duration: 1000
+              })
+          }
+        })
+      }
+    })
+  },
 
     /**
      * 生命周期函数--监听页面加载
