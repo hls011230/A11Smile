adcc22eedbf46f97d6d26d472ab767e3151a6a21
hls <13285986017@163.com>
2022-04-01 22:28:19 +0800

    hls 新增了识别医疗信息功能

--- golang/deploy/service/server.go
+++ golang/deploy/service/server.go
@@ -34,6 +34,7 @@ func Start(addr, contract_address string) (err error) {
 	user := r.Group("/user")
 	{
 		user.POST("/verifyIDCard", user_verifyIDCardHandler)
+		user.POST("/readMedicalInformation", user_readMedicalInformation)
 	}
 
 	// 征求者
