6bed6f029b6f7731b8b2094ac04468eee96810a8
xyj <1942049754@qq.com>
2022-04-27 01:07:31 +0800

    xyj 病历预览

--- mini_program/weapp/pages/user_report/user_report.js
+++ mini_program/weapp/pages/user_report/user_report.js
@@ -10,7 +10,7 @@ Page({
     data: {
         image: [],
         reports: [],
-        temp: []
+        fileName: []
     },
 
     getData() {
@@ -35,84 +35,111 @@ Page({
         })
     },
 
-    setName: function(){
+    setName: function () {
+        let _this = this
         wx.showModal({
-          editable: true,
-          placeholderText: '请输入报告名称',
-          showCancel: true,
-          title: '请给需要上传的报告名命',
-          success: (res) => {
-              console.log(res)
-          },
-          fail: (res) => {}
+            editable: true,
+            placeholderText: '请输入报告名称',
+            showCancel: true,
+            title: '请给需要上传的报告名命',
+            success: (res) => {
+                if (res.confirm == true && res.content != '') {
+                    let fileName = res.content
+                    //让用户选择或拍摄一张照片
+                    wx.chooseImage({
+                        count: 1,
+                        sizeType: ['original', 'compressed'],
+                        sourceType: ['album', 'camera'],
+                        success(res) {
+                            //选择完成会先返回一个临时地址保存备用
+                            const tempFilePaths = res.tempFilePaths
+                            _this.setData({
+                                image: tempFilePaths
+                            })
+                            wx.uploadFile({
+                                filePath: _this.data.image[0],
+                                name: 'uploadFile',
+                                url: 'https://test-allsmile-1687181-1310014865.ap-shanghai.run.tcloudbase.com/user/uploadMedicalExaminationReport?fileName=' + fileName,
+                                header: {
+                                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                                    'uid': app.globalData.uid
+                                },
+                                method: 'POST',
+                                // formData: {
+                                //     'user': 'test'
+                                // },
+                                success: function (res) {
+                                    if (res.data == '{"code":0,"data":"上传体检报告成功"}') {
+                                        wx.showToast({
+                                            title: '添加成功',
+                                            icon: 'success',
+                                            duration: 1500,
+                                            success: function () {
+                                                if (_this.data.reports == null) {
+                                                    _this.data.fileName.unshift(fileName)
+                                                    _this.setData({
+                                                        reports: _this.data.fileName
+                                                    })
+                                                } else {
+                                                    _this.data.reports.unshift(fileName)
+                                                    let newReports = _this.data.reports
+                                                    _this.setData({
+                                                        reports: newReports
+                                                    })
+                                                }
+                                            }
+                                        })
+                                    } else {
+                                        wx.showToast({
+                                            title: '添加失败',
+                                            icon: 'error',
+                                            duration: 1500,
+                                        })
+                                    }
+                                },
+                                fail: function (res) {
+                                    wx.showToast({
+                                        title: '添加失败',
+                                        icon: 'error',
+                                        duration: 1500,
+                                    })
+                                }
+                            })
+                        },
+                    })
+                }
+            }
         })
     },
 
-    uploadReport: function () {
-        var _this = this
-        //让用户选择或拍摄一张照片
-        wx.chooseImage({
-            count: 1,
-            sizeType: ['original', 'compressed'],
-            sourceType: ['album', 'camera'],
-            success(res) {
-                //选择完成会先返回一个临时地址保存备用
-                const tempFilePaths = res.tempFilePaths
-                _this.setData({
-                    image: tempFilePaths
-                })
-                wx.uploadFile({
-                    filePath: _this.data.image[0],
-                    name: 'uploadFile',
-                    url: 'https://test-allsmile-1687181-1310014865.ap-shanghai.run.tcloudbase.com/user/uploadMedicalExaminationReport',
-                    header: {
-                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        'uid': app.globalData.uid
-                    },
-                    method: 'POST',
-                    // formData: {
-                    //     'user': 'test'
-                    // },
-                    success: function (res) {
-                        if (res.data == '{"code":0,"data":"上传体检报告成功"}') {
-                            wx.showToast({
-                                title: '添加成功',
-                                icon: 'success',
-                                duration: 1500,
-                                success: function () {
-                                    let str = _this.data.image[0]
-                                    let tem = str.slice(11)
-                                    if (_this.data.reports == null) {
-                                        _this.data.temp.unshift(tem)
-                                        _this.setData({
-                                            reports: _this.data.temp
-                                        })
-                                    } else {
-                                        _this.data.reports.unshift(tem)
-                                        let newReports = _this.data.reports
-                                        _this.setData({
-                                            reports: newReports
-                                        })
-                                    }
-                                }
-                            })
-                        } else {
-                            wx.showToast({
-                                title: '添加失败',
-                                icon: 'error',
-                                duration: 1500,
-                            })
-                        }
-                    },
-                    fail: function (res) {
-                        wx.showToast({
-                            title: '添加失败',
-                            icon: 'error',
-                            duration: 1500,
-                        })
-                    }
-                })
+    previewImage(e) {
+        let _this = this
+        let fileName = e.currentTarget.dataset.filename
+        wx.cloud.callContainer({
+            "config": {
+                "env": "prod-9gy59jvo10e0946b"
             },
+            "path": "/user/previewMedicalExaminationReport",
+            "header": {
+                "X-WX-SERVICE": "test-allsmile",
+                "content-type": "application/json",
+                "uid": app.globalData.uid
+            },
+            "method": "POST",
+            "data": {
+                "file_name": fileName
+            },
+            success: function (res) {
+                let picture = []
+                if (res.data.data == undefined) {
+                    picture = _this.data.image
+                }else{
+                    picture[0] = res.data.data
+                }
+                wx.previewImage({
+                    urls: picture,
+                })
+            }
         })
     },
 
